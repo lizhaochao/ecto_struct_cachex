628e01da4eeb8602334de5b169ee2e7627fd963d
lizhaochao <lzc@lizhaochao.com>
2021-06-12 22:03:10 +0800

    refactor Cache

--- lib/cache.ex
+++ lib/cache.ex
@@ -6,10 +6,10 @@ defmodule ESC.Cache do
   ### Interface
   def get(struct_name, conds_or_id, exec_block) when is_atom(struct_name) do
     with(
-      {list, len, cap} <- get_list(struct_name),
+      {list, len, cap} <- get_data(struct_name),
       {cached_obj, list} <- get_obj(list, conds_or_id, len),
       {:ok, obj} <- exec_block_if_needed(cached_obj, exec_block),
-      [_ | _] <- save_list(list, cached_obj, obj, len, cap, struct_name)
+      [_ | _] <- save(struct_name, list, len, obj, cap, cached_obj)
     ) do
       {:ok, obj}
     else
@@ -17,11 +17,11 @@ defmodule ESC.Cache do
     end
   end
 
-  def put(struct_name, exec_block) do
+  def put(struct_name, exec_block) when is_atom(struct_name) do
     with(
       {:ok, %_{} = obj} <- exec_block.(),
-      {list, len, cap} <- get_list(struct_name),
-      [_ | _] <- save_list(list, nil, obj, len, cap, struct_name)
+      {list, len, cap} <- get_data(struct_name),
+      [_ | _] <- save(struct_name, list, len, obj, cap)
     ) do
       {:ok, obj}
     else
@@ -30,7 +30,7 @@ defmodule ESC.Cache do
   end
 
   ### Implements
-  def get_list(struct_name) do
+  def get_data(struct_name) do
     fn repo ->
       list = get_in(repo, [:db, struct_name]) || []
       len = get_in(repo, [:meta, :len, struct_name]) || 0
@@ -46,24 +46,32 @@ defmodule ESC.Cache do
   def exec_block_if_needed(nil = _obj, exec_block), do: exec_block.()
   def exec_block_if_needed(%_{} = obj, _exec_block), do: {:ok, obj}
 
-  def save_list(list, cached_obj, obj, len, cap, struct_name) do
+  def save(struct_name, list, len, obj, cap, cached_obj \\ nil) do
     with(
       {_del_id, list} <- make_list(list, obj, cached_obj),
       len <- make_len(len, cap, cached_obj)
     ) do
-      fn repo ->
-        repo = put_in(repo, [:db, struct_name], list)
-        new_repo = put_in(repo, [:meta, :len, struct_name], len)
-        {list, new_repo}
-      end
-      |> Repo.sync_update()
+      save(struct_name, list, len)
     end
+  end
+
+  def save(struct_name, list, len) do
+    fn repo ->
+      new_repo =
+        repo
+        |> put_in([:db, struct_name], list)
+        |> put_in([:meta, :len, struct_name], len)
+
+      {list, new_repo}
+    end
+    |> Repo.sync_update()
   end
 
   def make_list(list, obj, cached_obj \\ nil)
   def make_list(list, obj, nil = _cached_obj), do: LRU.put(list, obj)
   def make_list(list, _obj, _cached_obj), do: {nil, list}
 
+  def make_len(len, cap, cached_obj \\ nil)
   def make_len(len, cap, nil = _cached_obj) when len != cap, do: len + 1
   def make_len(len, _cap, _cached_obj), do: len
 end
